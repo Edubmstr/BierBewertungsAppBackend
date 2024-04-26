c7ae846bab44df281e6a1e10dbaa528cc5d9b194
Eduard Baumeister <eduard.baumeister1@gmail.com>
2024-04-26 14:03:18 +0200

    expires in

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ app.post('/validateuser', async (req,res) => {
             const userId = await getUserId(name);
             const token = jwt.sign(loginData, jwtSecretKey, {expiresIn: 60});
             const refreshToken = jwt.sign(loginData, jwtRefreshSecretKey);
-            res.cookie("refreshtoken", refreshToken, {httpOnly: true, expires: new Date(Date.now() + 900000), secure: true});
+            res.cookie("refreshtoken", refreshToken, {httpOnly: true, expires: new Date(Date.now() + 90000000), secure: true});
             return res.status(200).json({ message: "Succesfully logged in.", "token" : token, "userId": userId.user_id});
         }
     })
@@ -230,7 +230,7 @@ app.post('/refreshtoken', (req, res) => {
         const accessToken = jwt.sign(loginData, jwtSecretKey, {expiresIn: 60});
         const refreshToken = jwt.sign(loginData, jwtRefreshSecretKey);
 
-        res.cookie("refreshToken", refreshToken, {httpOnly: true, expires: new Date(Date.now() + 900000), secure: true}).status(200).json({message: "Token refreshed.", token: accessToken})
+        res.cookie("refreshToken", refreshToken, {httpOnly: true, expires: new Date(Date.now() + 90000000), secure: true}).status(200).json({message: "Token refreshed.", token: accessToken})
 })
 
 app.get('/latestentries', async (req, res) => {
