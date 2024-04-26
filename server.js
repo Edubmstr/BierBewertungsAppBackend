6fe0e01ce0dea24350f19f2c8f5a4bb34026e71c
Eduard Baumeister <eduard.baumeister1@gmail.com>
2024-04-26 13:32:20 +0200

    updated expire date cookie

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ app.post('/validateuser', async (req,res) => {
             const userId = await getUserId(name);
             const token = jwt.sign(loginData, jwtSecretKey, {expiresIn: 60});
             const refreshToken = jwt.sign(loginData, jwtRefreshSecretKey);
-            res.cookie("refreshtoken", refreshToken, {httpOnly: true});
+            res.cookie("refreshtoken", refreshToken, {httpOnly: true, expires: new Date(Date.now() + 900000)});
             return res.status(200).json({ message: "Succesfully logged in.", "token" : token, "userId": userId.user_id});
         }
     })
