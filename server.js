18c6a167b509cc4f2166879e5103aef432df4841
Edubmstr <153729455+Edubmstr@users.noreply.github.com>
2024-05-13 12:58:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import cookieParser from 'cookie-parser';
 import { getAllReviews, getSingleReviews, getUserReviews, createNewReview, createNewUser, checkIfUserNameExists, getUserPassword, getUserId, getLatestEntries, getCalculatedAverage, updateReview, deleteReview } from './prismatest.js';
 const app = express();
 const router = express.Router();
-app.use(cors({credentials: true, origin: 'http://127.0.0.1:3000'}));
+app.use(cors({credentials: true, origin: 'https://super-dieffenbachia-8a48cd.netlify.app'}));
 app.use(cookieParser());
 app.use(express.json());
 dotenv.config();
