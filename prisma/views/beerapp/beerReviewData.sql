80b3c74d2a6515dbec1b1110d2134661d1ab9884
Eduard Baumeister <eduard.baumeister1@gmail.com>
2024-06-13 11:53:58 +0200

    new db

---
+++ prisma/views/beerapp/beerReviewData.sql
@@ -0,0 +1,22 @@
+SELECT
+  `beerapp`.`beerReviews`.`review_id` AS `review_id`,
+  `beerapp`.`beerReviews`.`rating` AS `rating`,
+  `beerapp`.`beerReviews`.`longReview` AS `longReview`,
+  `beerapp`.`beerReviews`.`beerName` AS `beerName`,
+  `beerapp`.`beerReviews`.`created_at` AS `created_at`,
+  `beerapp`.`beerReviews`.`shortReview` AS `shortReview`,
+  `beerapp`.`beerReviews`.`picture_url` AS `picture_url`,
+  `beerapp`.`beerReviews`.`category` AS `category`,
+  `beerapp`.`beerReviews`.`brewery` AS `brewery`,
+  `beerapp`.`beerReviews`.`created_by_user_id` AS `created_by_user_id`,
+  `beerapp`.`beerAppUser`.`user_id` AS `user_id`,
+  `beerapp`.`beerAppUser`.`user_name` AS `user_name`
+FROM
+  (
+    `beerapp`.`beerReviews`
+    JOIN `beerapp`.`beerAppUser` ON(
+      (
+        `beerapp`.`beerReviews`.`created_by_user_id` = `beerapp`.`beerAppUser`.`user_id`
+      )
+    )
+  )
